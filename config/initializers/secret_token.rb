ff4c287e23fb0c426a580bcfc0e3e9a5c7069ba8
Iwabuchi Ken <iwabuchi.k.2010@gmail.com>
2013-04-20 09:50:17 +0900

    m
    v-1.0
    ---
    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CR6R::Application.config.secret_token = 'b10ae47c1b5901ebc31efd8e69b6a08ef4c0d7b91ffb36800842c77634649dc42ba30572ac75ef07a95101f229f0604c86f15e7c35a87707ae5100007574cfdb'
