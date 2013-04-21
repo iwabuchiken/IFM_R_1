2aa3ade0b4a8fe72fbe170b4d8f5f88d1623799d
Iwabuchi Ken <iwabuchi.k.2010@gmail.com>
2013-04-21 10:59:55 +0900

    B6
    v-1.0
    ---
    1. "updated_at" => Enter value when the instance created

--- app/models/word.rb
+++ app/models/word.rb
@@ -5,4 +5,29 @@ class Word < ActiveRecord::Base
   has_many :word_lists
   has_many :texts, through: :word_lists
 
+
+  before_save :add_created_at_millsec
+  before_update :update_created_at_millsec
+  
+  
+  protected
+  def add_created_at_millsec
+    
+    mill_sec = (Time.now.to_f * 1000.0).to_i
+    
+    self.created_at_mill = mill_sec
+    self.updated_at_mill = mill_sec
+    
+    # self.created_at_mill = (Time.now.to_f * 1000.0).to_i
+    
+  end#def add_created_at_millsec
+  
+  def update_created_at_millsec
+    
+    mill_sec = (Time.now.to_f * 1000.0).to_i
+    
+    self.updated_at_mill = mill_sec
+    
+  end#def update_created_at_millsec
+
 end
--- app/models/word_list.rb
+++ app/models/word_list.rb
@@ -5,12 +5,27 @@ class WordList < ActiveRecord::Base
 
   # => http://stackoverflow.com/questions/6584745/processing-data-before-saving-to-database
   before_save :add_created_at_millsec
+  before_update :update_created_at_millsec
   
   
   protected
   def add_created_at_millsec
     
-    self.created_at_mill = (Time.now.to_f * 1000.0).to_i
+    mill_sec = (Time.now.to_f * 1000.0).to_i
+    
+    self.created_at_mill = mill_sec
+    self.updated_at_mill = mill_sec
+    
+    # self.created_at_mill = (Time.now.to_f * 1000.0).to_i
     
   end#def add_created_at_millsec
+  
+  def update_created_at_millsec
+    
+    mill_sec = (Time.now.to_f * 1000.0).to_i
+    
+    self.updated_at_mill = mill_sec
+    
+  end#def update_created_at_millsec
+  
 end
