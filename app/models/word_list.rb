a4e74ab194c20311aac1b83926eeaf21a115cc7a
Iwabuchi Ken <iwabuchi.k.2010@gmail.com>
2013-05-01 13:22:26 +0900

    B13 CHANGE-3
    v-1.0
    ---
    1. Validate unique  => WordList
    	==> Done

--- app/models/word_list.rb
+++ app/models/word_list.rb
@@ -1,16 +1,36 @@
 class WordList < ActiveRecord::Base
   
+  #####################################
+  #
+  # Relations
+  #
+  #####################################
   belongs_to :text
   belongs_to :word
 
+  #####################################
+  #
+  # Before operations
+  #
+  #####################################
   # => http://stackoverflow.com/questions/6584745/processing-data-before-saving-to-database
   before_save :add_created_at_millsec
   before_save :add_updated_at_millsec
 
   before_update :add_updated_at_millsec
   # before_update :update_created_at_millsec
+  #####################################
+  #
+  # Validations
+  #
+  #####################################
+  validates_uniqueness_of :text_id, :scope => :word_id
   
-  
+  #####################################
+  #
+  # Methods
+  #
+  #####################################  
   protected
   def add_created_at_millsec
     
