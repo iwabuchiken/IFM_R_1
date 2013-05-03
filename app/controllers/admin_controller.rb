15e8b125d4e36e124fe9465ebdf170359cc431d7
Iwabuchi Ken <iwabuchi.k.2010@gmail.com>
2013-05-03 17:02:21 +0900

    B18-2
    v-1.0
    ---
    1. text id added to text_ids

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -58,6 +58,199 @@ class AdminController < ApplicationController
     
     logout("Starting... sub1")
     
+    param_destroy_word_list = params[:item_refactor_table_word_lists]
+    
+    param_refactor_text_ids = params[:item_refactor_text_ids]
+    
+    
+    if param_destroy_word_list != nil
+      
+      logout("param_destroy_word_list => " + param_destroy_word_list)
+    
+      _sub1__1_refactor_table_word_lists()
+    
+      flash[:notice] = "word_list object destroyed: " + @counter.to_s
+      
+    elsif param_refactor_text_ids != nil
+      
+      _sub1__2_refactor_text_ids()
+      
+      flash[:notice] = "text_ids updated: " + @counter.to_s
+      
+    else
+      
+      logout("param_destroy_word_list => nil")
+      
+    end
+
+
+      
+    
+    # flash[:notice] = "word_list object destroyed: " + @counter.to_s
+    # if params[:item_refactor_table_word_lists]
+#       
+      # flash[:notice] = "Refactoring word_lists"
+#       
+    # else
+#       
+      # flash[:notice] = "Welcome"
+#       
+    # end
+#
+    # _sub1_redirection()
+    
+  end#def sub1
+
+
+  def _sub1__2_refactor_text_ids()
+    
+    @counter = 0
+    
+    words = Word.all()
+    
+    msg = "(" + __FILE__ + ":" + __LINE__.to_s + ") " + 
+        "words.length.to_s => " + words.length.to_s
+
+    logout(msg)
+
+    for i in (0..(words.length - 1)) do
+      
+        word = words[i]
+      
+        if word != nil && word.text_ids != nil
+          
+          msg = "(" + __FILE__ + ":" + __LINE__.to_s + ") " + 
+            "word != nil && word.text_ids != nil"
+
+          logout(msg)
+
+          # if words[0] != nil && words[0].text_ids != nil
+          
+          text_ids = word.text_ids
+          
+          text_id = word.text_id
+          
+          # Validation => id number more than 0?
+          if not text_id > 0
+            
+            msg = "(" + __FILE__ + ":" + __LINE__.to_s + ") " + 
+              "Not text_id > 0"
+  
+            logout(msg)
+            
+            next
+            
+          end
+          
+          # Flag
+          is_in = false
+          
+          # text_ids contains the text_id ?
+          for j in (0..(text_ids.length - 1)) do
+            
+            if text_id == text_ids[j]
+              
+              is_in = true
+              
+              break
+              
+            end
+            
+          end#for j in (0..(text_ids.length - 1)) do
+          
+          # Judge => If text_ids contains text_id, then move to
+          # =>        the next word object
+          # =>      If doesn't contain, then do text << word
+          if is_in
+
+            msg = "(" + __FILE__ + ":" + __LINE__.to_s + ") " + 
+              "is_in => word.id=" + word.id.to_s +
+              "/text_id=" + text_id.to_s
+  
+            logout(msg)
+            
+            next
+            
+          else  # => if doesn't contain
+
+            msg = "(" + __FILE__ + ":" + __LINE__.to_s + ") " + 
+              "Not in => word.id=" + word.id.to_s +
+              "/text_id=" + text_id.to_s
+  
+            logout(msg)
+            
+            # => Build a text instance
+            # => If no instance found, then do the next word object(i.e. continue
+            # =>  the for loop)
+            begin
+              
+              msg = "(" + __FILE__ + ":" + __LINE__.to_s + ") " + 
+                "Finding a text instance ... => text_id=" + text_id.to_s
+                
+  
+              logout(msg)
+              
+              text = Text.find_by_id(text_id)
+              # text = Text.find(:all, :id => text_id)
+              # text = Text.find(text_id)
+              
+              text.words << word
+              # text << word
+              # word << text
+              
+              msg = "(" + __FILE__ + ":" + __LINE__.to_s + ") " + 
+                "word added to the text: text_id=" + text_id.to_s +
+                      "/word_id=" + word.id.to_s
+    
+              logout(msg)
+              
+              # logout("word added to the text: text_id=" + text_id.to_s +
+                      # "/word_id=" + word.id.to_s)
+              
+            rescue
+
+              msg = "(" + __FILE__ + ":" + __LINE__.to_s + ") " + 
+                "Finding text => Failed: id=" + text_id.to_s
+    
+              logout(msg)
+              
+              # logout("Finding text => Failed: id=" + text_id.to_s)
+              
+              next
+              
+            end
+            
+          end#if is_in
+          
+          # logout("words[0].text_ids.class.to_s => " + words[0].text_ids.class.to_s)
+          
+        else
+
+          msg = "(" + __FILE__ + ":" + __LINE__.to_s + ") " + 
+              "word => nil: id of i=" + i.to_s
+
+          logout(msg)
+          
+          # logout("word => nil: id of i=" + i.to_s)
+          
+        end#if word != nil && word.text_ids != nil
+    
+    end#for i in (0..(words.length - 1)) do
+    
+    # for i in (0..(words.length - 1)) do
+#       
+      # word = words[i]
+#       
+      # text_ids = word.text_ids
+#       
+#       
+#       
+    # end#for i in (0..(words.length - 1)) do
+    
+  end#def _sub1__2_refactor_text_ids()
+  
+  def _sub1__1_refactor_table_word_lists
+    
     word_lists = WordList.all()
     
     @counter = 0
@@ -98,6 +291,10 @@ class AdminController < ApplicationController
         # next
       end
       
+    end#for i in (0..(word_lists.length - 1)) do
+    
+    logout("for operation => Done: (Destoryed:" + @counter.to_s + ")")
+
       # if word == nil
 #         
         # word_list.destroy
@@ -105,26 +302,9 @@ class AdminController < ApplicationController
         # counter += 1
 #         
       # end
-      
-    end#for i in (0..(word_lists.length - 1)) do
     
-    logout("for operation => Done: (Destoryed:" + @counter.to_s + ")")
-    
-    flash[:notice] = "word_list object destroyed: " + @counter.to_s
-    # if params[:item_refactor_table_word_lists]
-#       
-      # flash[:notice] = "Refactoring word_lists"
-#       
-    # else
-#       
-      # flash[:notice] = "Welcome"
-#       
-    # end
-#
-    # _sub1_redirection()
-    
-  end#def sub1
-
+  end#def _sub1__1_refactor_table_word_lists
+  
   # def _sub1_redirection
 # 
     # respond_to do |format|
