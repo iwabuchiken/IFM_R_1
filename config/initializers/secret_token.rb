67447195323c67a575abec8ef70ba3666c8e3ecf
Iwabuchi Ken <iwabuchi.k.2010@gmail.com>
2013-05-17 12:08:25 +0900

    m
    v-1.0
    ---
    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IFMR1::Application.config.secret_token = '3f6486f365a35d8107dfd757d8b977fe6974e1921106cf67122a6d72365120b6ccceb9e0648869b1e858d621c28c4765eab23228c0d5519f2ef2267124b30f58'
